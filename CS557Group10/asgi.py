139f25d00b0a794f43d4c7f2b5223eb6e19595bb
Felix Koch <168679135+fkoch03@users.noreply.github.com>
2025-04-15 16:29:37 -0500

    Init

---
+++ CS557Group10/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CS557Group10 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CS557Group10.settings')
+
+application = get_asgi_application()
