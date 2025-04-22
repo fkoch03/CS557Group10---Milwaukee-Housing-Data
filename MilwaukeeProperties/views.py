79620e6ce47cc92ff747ea16691f04807d44aa51
Felix Koch <168679135+fkoch03@users.noreply.github.com>
2025-04-22 09:32:29 -0500

    Wrote basic get methods for home and property

--- MilwaukeeProperties/views.py
+++ MilwaukeeProperties/views.py
@@ -6,17 +6,27 @@ from django.views import View
 
 # Create your views here.
 class HomeView(View):
-    def get(self, request):
+    def get(self, request, user_id):
         sales = Sale.objects.all()
+        user = User.objects.get(id=user_id)
         return render(request, 'home.html', {
             'sales': sales,
+            'user':user
         })
 
 class LoginView(View):
     def get(self, request):
 
 class PropertyView(View):
-    def get(self, request):
+    def get(self, request, property_id):
+        sales = Sale.objects.filter(property_id=property_id)
+        prop = Property.objects.get(id=property_id)
+        comments = Comment.objects.filter(property_id=property_id)
+        return render(request, 'property.html', {
+            'sales': sales,
+            'prop': prop
+            'comments': comments
+        })
 
 class SearchView(View):
     def get(self, request):
