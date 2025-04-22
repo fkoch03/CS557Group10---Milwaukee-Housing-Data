125662aa7b50168074575897b589a980271bb30c
Felix Koch <168679135+fkoch03@users.noreply.github.com>
2025-04-22 08:37:03 -0500

    Added models and got most of the settings to work with mysql

--- MilwaukeeProperties/models.py
+++ MilwaukeeProperties/models.py
@@ -1,3 +1,155 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=45, unique=True)
+    email = models.CharField(max_length=45, null=True, blank=True)
+    first_name = models.CharField(max_length=45, null=True, blank=True)
+    last_name = models.CharField(max_length=45, null=True, blank=True)
+
+
+
+class Location(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    zip = models.IntegerField(null=True, blank=True)
+    latitude = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True)
+
+
+class CondoProject(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, null=True, blank=True)
+
+
+class Alderman(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=45, null=True, blank=True)
+    last_name = models.CharField(max_length=45, null=True, blank=True)
+    date_elected = models.DateTimeField(null=True, blank=True)
+
+
+class District(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, null=True, blank=True)
+    alderman = models.ForeignKey(
+        Alderman,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+
+
+class Neighborhood(models.Model):
+    nbhd = models.CharField(max_length=4, primary_key=True)
+    emperor = models.CharField(max_length=45, null=True, blank=True)
+
+
+class Property(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=45, null=True, blank=True)
+    taxkey = models.CharField(max_length=45, null=True, blank=True)
+    location = models.ForeignKey(
+        Location,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    condo = models.ForeignKey(
+        CondoProject,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    district = models.ForeignKey(
+        District,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    nbhd = models.ForeignKey(
+        Neighborhood,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING
+    )
+    style = models.CharField(max_length=45, null=True, blank=True)
+    extwall = models.CharField(max_length=45, null=True, blank=True)
+    stories = models.DecimalField(max_digits=1, decimal_places=0, null=True, blank=True)
+    year_built = models.IntegerField(null=True, blank=True)
+    rooms = models.IntegerField(null=True, blank=True)
+    finished_sqft = models.IntegerField(null=True, blank=True)
+    units = models.IntegerField(null=True, blank=True)
+    full_bath = models.IntegerField(null=True, blank=True)
+    half_bath = models.IntegerField(null=True, blank=True)
+    lot_size = models.IntegerField(null=True, blank=True)
+
+
+class RealtorCompany(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, null=True, blank=True)
+
+
+class Realtor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=45, null=True, blank=True)
+    last_name = models.CharField(max_length=45, null=True, blank=True)
+    company = models.ForeignKey(
+        RealtorCompany,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING
+    )
+
+
+class Sale(models.Model):
+    id = models.IntegerField(primary_key=True)
+    property = models.ForeignKey(
+        Property,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING
+    )
+    realtor = models.ForeignKey(
+        Realtor,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    price = models.CharField(max_length=45, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+
+
+class Favorite(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    property = models.ForeignKey(
+        Property,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    date_added = models.DateTimeField(null=True, blank=True)
+
+
+class Comment(models.Model):
+    id = models.IntegerField(primary_key=True)
+    comment = models.CharField(max_length=280, null=True, blank=True)
+    user = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING
+    )
+    property = models.ForeignKey(
+        Property,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING
+    )
+    date = models.DateTimeField(null=True, blank=True)
